ca5c3eebeca75e6b31ff16058b5896c4dafb2fe9
Poke714 <sebastian714@t-online.de>
2017-10-24 20:52:24 +0200

    Button to hide touchscreen plays and new play

--- osu!topplays/script.js
+++ osu!topplays/script.js
@@ -1,13 +1,27 @@
+var list;
+var touchscreen = true;
+
 function refresh() {
     var options = {
         valueNames: ["pp", "player", "map", "mods", "acc", "rank", "status", "date"],
         item: "<tr><td></td><td class='pp'></td><td class='player'></td><td class='map'></td><td class='mods'><td class='acc'></td><td class='rank'></td><td class='status'></td><td class='date'></td></tr>"
     };
 
-    var list = new List("plays", options, values);
+    list = new List("plays", options, values);
     
     list.sort("pp", { order: "desc" });
     
     // TODO rank alphabet = "SABCD"
-    // TODO button to filter touchscreen plays (http://listjs.com/api/#filter)
+}
+
+
+function filterTouchscreen() {
+    touchscreen = !touchscreen;
+    list.filter(function(item) {
+        if(!touchscreen)
+            return item.values().touchscreen != "true";
+        else return true;
+    });
+    console.log(touchscreen)
+    document.getElementById("touchscreen").innerHTML = touchscreen ? "Hide touchscreen plays" : "Show touchscreen plays"
 }
